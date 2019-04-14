7b0fbd6b541ba6713f8aa670a51f9b7a4aa10e70
Somesh99 <49450623+Somesh99@users.noreply.github.com>
2019-04-14 21:18:57 +0530

    Update my os.pro.c

--- my os.pro.c
+++ my os.pro.c
@@ -1,38 +1,53 @@
 #include<stdio.h>
 #include<conio.h>
-#include<string.h>
-#include<time.h>
-#include<stdlib.h>
 
 int main()
 {   int n;
     printf("Enter No... Of Process :::  ");
     scanf("%d",&n);
-	int i,j,Proc[n],ariv_t[n],burst_t[n],Prio[n],wait_t[n],trnar_t[n];
+	int i,j,proc[n],ariv_t[n],burst_t[n],Prio[n],wait_t[n],trnar_t[n],min;
 	
-	printf("\nEnter Burst-Time And Arrival-Time Of Each Process (In Integer Seconds) :::\n");
+	printf("\nEnter Burst--Time (In Integer Seconds) :::\n");
 	for(i=0;i<n;i++)
 	{
 		printf("\nprocs%d\t",(i+1));
 		scanf("%d",&burst_t[i]);
-		scanf("%d",&ariv_t[i]);
-		 Proc[i]=i+1;
 	}
-        wait_t[0]=0;
+	printf("\n\nEnter Arrival--Time (In Integer Seconds) :::\n");
+	{
+		for(i=0;i<n;i++)
+	{
+		printf("\nprocs%d\t",(i+1));
+		scanf("%d",&ariv_t[i]);
+	
+	}
+	}
+	wait_t[0]=0;
     for(j=0;j<n;j++)
 	{
-	  wait_t[j+1]=wait_t[j]+burst_t[j];	
-	  
+	  wait_t[j+1]=wait_t[j]+burst_t[j];	 
 	}
 	
-	for(j=0;j<n;j++)
+	for(i=0;i<n;i++)
 	{
-	  trnar_t[j]=wait_t[j]+burst_t[j];	
+	 for(j=0;j<n-i-1;j++)
+	 {
+	   if(ariv_t[j]>ariv_t[j+1])
+	   {
+	  	min=ariv_t[j];
+	  	ariv_t[j]=ariv_t[j+1];
+	  	ariv_t[j+1]=min;
+	  	//proc[j]=min;
+	   }
+     }
 	}
-	
-	for(j=0;j<n;j++)
-	{
-	  printf("\n%d \t %d" ,wait_t[j],trnar_t[j])	;
+    for(i=0;i<n;i++)
+    {
+    	printf("%d",ariv_t[i]);
+    	printf("%d",proc[i]);
 	}
 }
+
+        
+
 //this is my os project made by me...
