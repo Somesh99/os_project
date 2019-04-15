2b0850c59b872df9549bec023a1f725385fb7c23
Somesh99 <49450623+Somesh99@users.noreply.github.com>
2019-04-15 21:52:36 +0530

    Update my os.pro.c

--- my os.pro.c
+++ my os.pro.c
@@ -2,52 +2,40 @@
 #include<conio.h>
 
 int main()
-{   int n;
-    printf("Enter No... Of Process :::  ");
-    scanf("%d",&n);
-	int i,j,proc[n],ariv_t[n],burst_t[n],Prio[n],wait_t[n],trnar_t[n],min;
-	
-	printf("\nEnter Burst--Time (In Integer Seconds) :::\n");
-	for(i=0;i<n;i++)
-	{
-		printf("\nprocs%d\t",(i+1));
-		scanf("%d",&burst_t[i]);
-	}
-	printf("\n\nEnter Arrival--Time (In Integer Seconds) :::\n");
-	{
-		for(i=0;i<n;i++)
-	{
-		printf("\nprocs%d\t",(i+1));
-		scanf("%d",&ariv_t[i]);
-	
-	}
-	}
-	wait_t[0]=0;
-    for(j=0;j<n;j++)
-	{
-	  wait_t[j+1]=wait_t[j]+burst_t[j];	 
-	}
-	
-	for(i=0;i<n;i++)
-	{
-	 for(j=0;j<n-i-1;j++)
-	 {
-	   if(ariv_t[j]>ariv_t[j+1])
-	   {
-	  	min=ariv_t[j];
-	  	ariv_t[j]=ariv_t[j+1];
-	  	ariv_t[j+1]=min;
-	  	//proc[j]=min;
-	   }
-     }
-	}
-    for(i=0;i<n;i++)
+{   
+  int n,i,j,min,pos;
+  scanf("%d",&n);
+  int a[n],b[n];
+  for(i=0;i<n;i++)
+  {
+  	scanf("%d",&a[i]);
+  	b[i]=i+1;
+  }
+  
+  for(i=0;i<n;i++)
     {
-    	printf("%d",ariv_t[i]);
-    	printf("%d",proc[i]);
-	}
-}
-
-        
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[pos])
+               pos=j;
+        }  
+        min=a[i];
+        a[i]=a[pos];
+        a[pos]=min;  
+        min=b[i];
+        b[i]=b[pos];
+        b[pos]=min;
+    } 
+   system("cls");
+   printf("\n");
+   system("color 5E");
+   printf("PROCESS\t\tBURST_T\n");
+   for(i=0;i<n;i++)
+   {
+   	printf(" Pr %d\t\t  %d",b[i],a[i]);
+   	printf("\n");
+   	
+   }	
 
 //this is my os project made by me...
