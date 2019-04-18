a49eb2a74c11d42a7345fdc89f2e7021718a9dc9
Somesh99 <49450623+Somesh99@users.noreply.github.com>
2019-04-18 10:09:47 +0530

    Update my os.pro.c

--- my os.pro.c
+++ my os.pro.c
@@ -1,32 +1,52 @@
 #include<stdio.h>
 #include<conio.h>
-
+#include<time.h>
 int main()
 {   
-  int n;
+  int n,bit,but;
     printf("Enter No... Of Process :::  ");
     scanf("%d",&n);
-	int i,j,p[n],at[n],bt[n],Prio[n],wt[n],trt[n],min,pos,min1,min2,pos1,min3;
-	
+	int i,j,p[n],at[n],bt[n],Prio[n],wt[n],tat[n],min,pos,min1,min2,pos1,min3,tot_wt=0,tot_tat=0;
+    bit:
 	printf("\nEnter Burst--Time (In Integer Seconds) :::\n");
 	for(i=0;i<n;i++)
 	{
 		printf("\nprocs%d\t",(i+1));
 		scanf("%d",&bt[i]);
 	}
-	printf("\n\nEnter Arrival--Time (In Integer Seconds) :::\n");
+	for(i=0;i<n;i++)
 	{
-		for(i=0;i<n;i++)
+		if(bt[i]<=0)
+		{
+			printf("\n\nINVALID-BURST_TIME :");
+			sleep(8);
+			system("cls");
+			goto bit;
+		}
+	}
+	but:
+	printf("\n\nEnter Arrival--Time (In Integer Seconds) :::\n");
+	
+	for(i=0;i<n;i++)
 	{
 		printf("\nprocs%d\t",(i+1));
 		scanf("%d",&at[i]);
 	    p[i]=i+1;
 	}
+	for(i=0;i<n;i++)
+	{
+		if(at[i]<0)
+		{
+			printf("\n\nINVALID-ARIVAL_TIME :");
+			sleep(8);
+			system("cls");
+			goto but;
+		}
 	}
-  /////////////////
   
   
-  for(i=0;i<n;i++)
+  
+   for(i=0;i<n;i++)
     {
         pos=i;
         for(j=i+1;j<n;j++)
@@ -85,25 +105,25 @@ int main()
    	printf("\n");
    	
    }
- 
-
-   // wt[0]=0;
-   // tot_wt=0;
-    //for(i=0;i<n;i++)
-  //  {
-   // 	bt
-//	}
-  //  for(i=0;i<n;i++)
-   // {
-    //	wt[i]=wt[i]+bt[i];
-    //	tat[i]=bt[i]+wt[i]; 
-    //	tot_wt=tot_wt+wt[i];
-    //	tot_tat=tot_tat+tat[i];
-//	}
-
-    
+    wt[0]=0;
+    for(i=0;i<n-1;i++)
+    {
+    	wt[i+1]=wt[i]+bt[i+1];
+    	tot_wt=tot_wt+wt[i];
+	}
+	for(i=0;i<n;i++)
+	{
+		tat[i]=bt[i]+wt[i]; 
+    	tot_tat=tot_tat+tat[i];
+	}
+    printf("\n");
+    printf("PROCESS\t\tARIVAL_T\tBURST_T\t\t TURNAR_T\t\t WAITING_T\n");
+    for(i=0;i<n;i++)
+    {
+      printf(" Pr %d\t\t %d\t\t %d\t\t  %d\t\t\t  %d\t\t",p[i],at[i],bt[i],tat[i],wt[i]);
+   	  printf("\n");	
+	}
 
     
   
 }
-
